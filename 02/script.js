c34ea219226c7b291c5932cc326a6fc6be74d7cc
abhay <abhay_b220091cs@nitc.ac.in>
2026-04-06 16:22:03 +0530

    first one

---
+++ 02/script.js
@@ -0,0 +1,18 @@
+const { useDebugValue } = require("react");
+
+var age =20;
+var age1=10;
+console.log(age + age1);
+
+// js is loosely type language dynamically allocate
+// var scope is global - though out program if at what ever used 
+
+
+// console.log(tp);
+// var tp=20;
+// shows undefined
+
+let tp=100;
+// let scope is local scope
+
+console.log("from external") 
